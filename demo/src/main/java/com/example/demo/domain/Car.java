74577d5b94dd67d68eb3fa9865f9e8b7165b8cb8
topiaswho <topias.hoyhtya@outlook.com>
2023-11-02 12:59:25 +0200

    Ekat lisäykset

---
+++ demo/src/main/java/com/example/demo/domain/Car.java
@@ -0,0 +1,52 @@
+package com.example.demo.domain;
+
+public class Car {
+	
+	private String brand;
+    private String model;
+    private int year;
+    
+    public Car() {
+        this.brand = null;
+        this.model = null;
+        this.year = 0;
+
+    }
+    
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+
+    }
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [brand=" + brand + ", model=" + model + ", year=" + year + "]";
+	}
+    
+}
